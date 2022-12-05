b7b9952869899200ece1e7a0abc72231c79ba026
Królewicz <bartek.konon2@wp.pl>
2022-12-05 14:28:58 +0100

    Dodaj pliki projektów.

---
+++ Class_na_temat_aut/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Class_na_temat_aut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochód Samochód1 = new Samochód();
+           
+            Samochód1.Marka = "BMW M4";
+            Samochód1.Cena = 750000;
+            
+        }
+    }
+
+    class Samochód
+    {
+        public string Marka;
+        public string Kolor;
+        public int Cena;
+        public int Dataprodukcji;
+        public int Prędkość;
+
+        public Samochód(string Marka, int Cena)
+        {
+            Console.WriteLine(Marka);
+            Console.WriteLine(Cena);
+        }
+
+        public Samochód(string Marka, string Kolor, int Cena, int Dataprodukcji)
+        {
+            Console.WriteLine(Marka);
+            Console.WriteLine(Kolor);
+            Console.WriteLine(Cena);
+            Console.WriteLine(Dataprodukcji);
+        }
+    }
+}
